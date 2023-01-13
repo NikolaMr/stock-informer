798267dfeb936b2085fe9dd10ff67a49a5614ac7
Nikola Mrzljak <nikolamrzljak@yahoo.com>
2023-01-13 12:04:43 +0100

    Added health check endpoint, minor refactor, added exception on missing ticker symbol, added descriptions for endpoints.

--- stock_informer_server.py
+++ stock_informer_server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from stock_informer_factory import StockInformerFactory
 from models import StockDetails
 
@@ -14,13 +14,47 @@ async def startup():
     FastAPICache.init(InMemoryBackend(), prefix="fastapi-cache")
 
 
-@app.get("/stocks/details/{stock_ticker_symbol}", response_model=StockDetails)
+@app.get(
+    "/stocks/details/{stock_ticker_symbol}",
+    response_model=StockDetails,
+    description='This endpoint fetches details for a stock with the given ticker symbol.',
+    responses={
+        404: {
+            'description': 'Ticker symbol not found.'
+        }
+    }
+)
 @cache(expire=60)
 async def stock_details(stock_ticker_symbol: str):
-    return StockInformerFactory.get_stock_informer().get_stock_details(stock_ticker_symbol)
+    details = StockInformerFactory.get_stock_informer().get_stock_details(stock_ticker_symbol)
+    if details is not None:
+        return details
+    raise HTTPException(status_code=404, detail='Ticker symbol not found.')
 
 
-@app.get("/stocks/price/{stock_ticker_symbol}", response_model=float)
+@app.get(
+    "/stocks/price/{stock_ticker_symbol}",
+    response_model=float,
+    description='This endpoint fetches the price of the stock with the given ticker symbol',
+    responses={
+        404: {
+            'description': 'Ticker symbol not found.'
+        }
+    }
+)
 @cache(expire=60)
 async def stock_price(stock_ticker_symbol: str):
-    return StockInformerFactory.get_stock_informer().get_stock_price(stock_ticker_symbol)
+    price = StockInformerFactory.get_stock_informer().get_stock_price(stock_ticker_symbol)
+    if price is not None:
+        return price
+    raise HTTPException(status_code=404, detail='Ticker symbol not found.')
+
+
+@app.get(
+    "/health",
+    response_model=str,
+    description='This endpoint can be reached out to check if the server is working as expected. '
+                'For now, it just returns "working" to indicate so.'
+)
+async def health_check():
+    return 'working'
